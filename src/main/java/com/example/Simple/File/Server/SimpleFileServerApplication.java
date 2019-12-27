ee971d590fc249d0476f380b1a8631ff677ba1c6
Jilson <4jilson@gmail.com>
2019-12-27 22:02:09 +0530

    initial application from spring initializr

---
+++ src/main/java/com/example/Simple/File/Server/SimpleFileServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Simple.File.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFileServerApplication.class, args);
+	}
+
+}
