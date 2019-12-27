bc2e14e0d0485595eb7bc124db58bdb12c31c772
Jilson <4jilson@gmail.com>
2019-12-27 22:20:53 +0530

    application at default port 8080 file server at port 8084 serving file system with current directory as root

--- src/main/java/com/example/Simple/File/Server/SimpleFileServerApplication.java
+++ src/main/java/com/example/Simple/File/Server/SimpleFileServerApplication.java
@@ -1,13 +1,23 @@
 package com.example.Simple.File.Server;
 
+import io.undertow.Undertow;
+import io.undertow.server.handlers.resource.PathResourceManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static io.undertow.Handlers.resource;
+
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class SimpleFileServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleFileServerApplication.class, args);
+		Undertow server = Undertow.builder().addHttpListener(8084, "localhost")
+				.setHandler(resource(new PathResourceManager(Paths.get(System.getProperty("user.dir")), 100))
+						.setDirectoryListingEnabled(true))
+				.build();
+		server.start();
 	}
 
 }
